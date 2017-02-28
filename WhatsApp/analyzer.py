81147867bf985cff0dedfd4bcf1c0c597f31f243
Kunal Patel <kunalpatelx92@gmail.com>
2017-02-28 13:36:33 -0600

    Define Inputs & Outputs

---
+++ WhatsApp/analyzer.py
@@ -0,0 +1,9 @@
+# WhatsApp Chat Analyzer
+# Input: Chat text file
+# Output:
+          # For Each Person:
+            # Number of Messages
+            # Number of Words
+            # Top 20 Most Used Words
+            # Number of Questions
+            # 1 Word Replies
